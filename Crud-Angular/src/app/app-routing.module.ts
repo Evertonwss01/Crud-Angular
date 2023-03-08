ef4fb168577ff0af222d1b48c9122d5edd34a457
Evertonwss01 <evertonwss2001@gmail.com>
2023-03-08 10:30:18 -0300

    Configurando a rota do Modulos da Lista de Cursos

--- Crud-Angular/src/app/app-routing.module.ts
+++ Crud-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* Configurar o redirecinamento para Lista */
+  {path: '', pathMatch: 'full', redirectTo: 'lista'},
+  /* Carregamento lazi load, carregamento lento */
+  {
+    path: 'lista',
+    loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
